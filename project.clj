f485929b41ff7352fd89c07f7788b865ddee4b16
Paul Bostrom <pbostrom@roomkey.com>
2014-07-08 11:48:08 -0400

    convert to cljsasset library

--- project.clj
+++ project.clj
@@ -1,19 +1,18 @@
-(defproject om-codemirror "0.1.0-SNAPSHOT"
+(defproject om-codemirror "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2197"]
+                 [org.clojure/clojurescript "0.0-2227"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+                 [org.webjars/codemirror "4.3"]
                  [om "0.6.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
-
-  :cljsbuild { 
-    :builds [{:source-paths ["src"]
-              :compiler {
-                :output-to "app.js"
-                :output-dir "out"
-                :optimizations :none
-                :source-map true}}]})
+  :cljsasset {:js
+              ["META-INF/resources/webjars/codemirror/4.3/lib/codemirror.js"
+               "META-INF/resources/webjars/codemirror/4.3/mode/clojure/clojure.js"
+               "META-INF/resources/webjars/codemirror/4.3/addon/edit/closebrackets.js"
+               "META-INF/resources/webjars/codemirror/4.3/addon/edit/matchbrackets.js"]
+              :css
+              ["META-INF/resources/webjars/codemirror/4.3/lib/codemirror.css"]})
--- src/om_codemirror/core.cljs
+++ src/om_codemirror/core.cljs
@@ -1,12 +1,8 @@
-(ns om-codemirror.app
+(ns om-codemirror.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.core.async :refer [put! <! chan]]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
-
-(enable-console-print!)
-
-(def app-state (atom {}))
 
 (defn map-hotkey [e]
   (when (and (.-ctrlKey e) (.-shiftKey e))
@@ -24,21 +20,3 @@
     om/IDidMount
     (did-mount [_]
       (js/CodeMirror (om/get-node owner) #js {:matchBrackets true :autoCloseBrackets true}))))
-
-(defn cm-app [app owner]
-  (reify
-    om/IWillMount
-    (will-mount [_]
-      (let [comm (chan)]
-        (om/set-state! owner :comm comm)
-        (go (while true
-              (let [e (<! comm)]
-                (println "hotkey" e))))))
-    om/IRenderState
-    (render-state [_ {:keys [comm]}]
-      (dom/div nil
-        (dom/div #js {:className "cm-box"}
-          (om/build editor app
-            {:init-state {:comm comm}}))))))
-
-(om/root cm-app app-state {:target (.getElementById js/document "cmapp")})
