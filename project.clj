26cb54682c2c6cb1b284fba71ae715d58d8e9ccc
Paul Bostrom <pbostrom@roomkey.com>
2014-07-08 12:39:55 -0400

    use local om snapshot for now

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/clojurescript "0.0-2227"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [org.webjars/codemirror "4.3"]
-                 [om "0.6.0"]]
+                 [om "0.6.5-SNAPSHOT"]]
 
   :cljsasset {:js
               ["META-INF/resources/webjars/codemirror/4.3/lib/codemirror.js"
