557e060b2c12cfcf445a12888346f7ff5b1b00ce
Paul Bostrom <pbostrom@roomkey.com>
2014-10-16 09:12:44 -0400

    Bump dependencies, add url and description

--- project.clj
+++ project.clj
@@ -1,18 +1,17 @@
-(defproject om-codemirror "0.2.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject om-codemirror "0.2.0"
+  :description "Om wrapper around the CodeMirror JavaScript text editor"
+  :url "https://github.com/pbostrom/om-codemirror"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2227"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [org.webjars/codemirror "4.3"]
-                 [om "0.6.5-SNAPSHOT"]]
-
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [org.webjars/codemirror "4.6"] ; JavaScript library available on WebJars
+                 [pbostrom/om "0.7.3-SNAPSHOT"] ; Available on clojars
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :cljsasset {:js
-              ["META-INF/resources/webjars/codemirror/4.3/lib/codemirror.js"
-               "META-INF/resources/webjars/codemirror/4.3/mode/clojure/clojure.js"
-               "META-INF/resources/webjars/codemirror/4.3/addon/edit/closebrackets.js"
-               "META-INF/resources/webjars/codemirror/4.3/addon/edit/matchbrackets.js"]
+              ["META-INF/resources/webjars/codemirror/4.6/lib/codemirror.js"
+               "META-INF/resources/webjars/codemirror/4.6/mode/clojure/clojure.js"
+               "META-INF/resources/webjars/codemirror/4.6/addon/edit/closebrackets.js"
+               "META-INF/resources/webjars/codemirror/4.6/addon/edit/matchbrackets.js"]
               :css
-              ["META-INF/resources/webjars/codemirror/4.3/lib/codemirror.css"]})
+              ["META-INF/resources/webjars/codemirror/4.6/lib/codemirror.css"]})
