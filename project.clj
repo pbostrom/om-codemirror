25f0a2d640e5106843f15d1373a640f6d7bb66e5
Paul Bostrom <pbostrom@roomkey.com>
2014-10-16 10:58:57 -0400

    change om dep

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2322"]
                  [org.webjars/codemirror "4.6"] ; JavaScript library available on WebJars
-                 [pbostrom/om "0.7.3-SNAPSHOT"] ; Available on clojars
+                 [pbostrom/om "0.7.3"] ; Available on clojars
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :cljsasset {:js
               ["META-INF/resources/webjars/codemirror/4.6/lib/codemirror.js"
