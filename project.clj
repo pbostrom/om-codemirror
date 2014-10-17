16418163a5088a9e7260c34d6563f6fef22bac80
Paul Bostrom <pbostrom@roomkey.com>
2014-10-17 10:07:01 -0400

    Use original Om dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-codemirror "0.2.0"
+(defproject om-codemirror "0.2.1"
   :description "Om wrapper around the CodeMirror JavaScript text editor"
   :url "https://github.com/pbostrom/om-codemirror"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2322"]
                  [org.webjars/codemirror "4.6"] ; JavaScript library available on WebJars
-                 [pbostrom/om "0.7.3"] ; Available on clojars
+                 [om "0.7.3"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :cljsasset {:js
               ["META-INF/resources/webjars/codemirror/4.6/lib/codemirror.js"
